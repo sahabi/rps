e5391d7ffd3b88b7150f25d4876fe3c7a6695fb7
Mohammed Alshiekh <sahabi@gmail.com>
2017-12-31 23:41:40 -0600

    implementing move remains for completion

--- funcs.m
+++ funcs.m
@@ -12,13 +12,13 @@ function [s] = c2d(array)
   s = dx + (dy*dgrid_size(1));
 end
 
-function [array] = d2c(s);
+function [array] = d2c(s)
   dgrid_size = [4 4];
   cgrid_size = [2 2];
-  xwidth = cgrid_size(1)/dgrid_size(1);
-  ywidth = cgrid_size(2)/dgrid_size(2);  
+  xwidth = cgrid_size(1)/dgrid_size(1);  
+  ywidth = cgrid_size(2)/dgrid_size(2);
   x = (floor(mod(s, dgrid_size(1)))*xwidth) + (xwidth/2);
-  y = floor(s / dgrid_size(2));
+  y = floor(s / dgrid_size(2))*ywidth + ywidth/2;
   array = [x-1 y-1 0];
 end
 
--- reactive_ctrl.m
+++ reactive_ctrl.m
@@ -1,26 +1,14 @@
-%Initializing the agents to random positions with barrier certificates 
-%and data plotting.  This script shows how to initialize robots to a
-%particular point
-%Paul Glotfelter 
-%3/24/2016
-
-dgrid_size = [4,4];
-cgrid_size = [2,2];
+dgrid_size = [4.,4.];
+cgrid_size = [2,1.5];
 xwidth = cgrid_size(1)/dgrid_size(1);
 ywidth = cgrid_size(2)/dgrid_size(2);
-
-% Get the number of available agents from the Robotarium.  We don't need a
-% specific value for this algorithm
 N = 2;
 obj_next = zeros(15,3);
 obj_next(14,:) = [14,10,99];
 obj_next(10,:) = [6,10,14];
 obj_next(6,:) = [2,6,10];
 obj_next(2,:) = [2,6,99];
-% Get Robotarium object used to communicate with the robots/simulator
 rb = RobotariumBuilder();
-% Set the number of agents and whether we would like to save data.  Then,
-% build the Robotarium simulator object!
 r = rb.set_number_of_agents(N).set_save_data(false).build();
 init = false;
 init_dest_0 = d2c(0);
@@ -31,30 +19,34 @@ args = {'PositionError', 0.01, 'RotationError', 50};
 init_checker = create_is_initialized(args{:});
 controller = create_si_position_controller();
 si_to_uni_dynamics = create_si_to_uni_mapping2();
+x = r.get_poses();
 for k = 0:1000000
   si_vel = zeros(2,N);
-  x = r.get_poses();
   x_si = x(1:2, :);
   if ~init
     dest_pose(:,1) = init_dest_0;
     dest_pose(:,2) = init_dest_1;
-    while(sum(init_checker(x,dest_pose))~=N)
+    dest_pose(3,:) = x(3,:);
+    while(~init_checker(x,dest_pose))
       si_velocities = controller(x(1:2, :), dest_pose(1:2, :));
       si_velocities = si_barrier_certificate(si_velocities, x(1:2, :));
       uni_velocities = si_to_uni_dynamics(si_velocities, x);
       r.set_velocities(1:N, uni_velocities);
       r.step();
       x = r.get_poses();
+      dest_pose(3,:) = x(3,:);
     end
     init = true;
   else
-    x = r.get_poses();
     x1 = x(1:2,2);
     dest_pose(:,1) = d2c(move(c2d(x1)));
-    dest_pose(:,2) = d2c(random.choice(ob_next(c2d(x1))));
+    x2 = x(1:2,1);
+    rs = randsample(ob_next(c2d(x2)),1);
+    dest_pose(:,2) = d2c(rs);
+    randsample(ob_next(c2d(x1)),1);
+    dest_pose(3,:) = x(3,:);
   end
-
-  while (sum(init_checker(x,dest_pose))~=N)
+  while (~init_checker(x,dest_pose))
       si_velocities = controller(x(1:2, :), dest_pose(1:2, :));
       uni_velocities = si_to_uni_dynamics(si_velocities, x);
       r.set_velocities(1:N, uni_velocities);
@@ -62,26 +54,6 @@ for k = 0:1000000
       x = r.get_poses();
   end
 end
-
-% We'll make the rotation error huge so that the initialization checker
-% doesn't care about it
-%args = {'PositionError', 0.01, 'RotationError', 50};
-%init_checker = create_is_initialized(args{:});
-%controller = create_si_position_controller();
-
-%while(~init_checker(x, initial_conditions))
-
-%    x = r.get_poses();
-%    dxi = controller(x(1:2, :), initial_conditions(1:2, :));
-%    dxi = si_barrier_certificate(dxi, x(1:2, :));      
-%    dxu = si_to_uni_dynamics(dxi, x);
-%
-%    r.set_velocities(1:N, dxu);
-%    r.step();   
-%end
-
-% Though we didn't save any data, we still should call r.call_at_scripts_end() after our
-% experiment is over!
 r.call_at_scripts_end();
 
 
